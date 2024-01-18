d9b14bd0ed56048eadedbbcb334a85953717c6e1
Supakrit Nithikethkul <6410615139@cn332>
2024-01-18 17:53:24 +0700

    Hello rust

---
+++ hello-rust/target/debug/deps/hello_rust-5314597c8b86d552.d
@@ -0,0 +1,5 @@
+/Users/kitt/Documents/3-2/CN408-ST/open3d-01/hello-rust/target/debug/deps/hello_rust-5314597c8b86d552: src/main.rs
+
+/Users/kitt/Documents/3-2/CN408-ST/open3d-01/hello-rust/target/debug/deps/hello_rust-5314597c8b86d552.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/kitt/Documents/3-2/CN408-ST/open3d-01/hello-rust/target/debug/hello-rust: /Users/kitt/Documents/3-2/CN408-ST/open3d-01/hello-rust/src/main.rs
